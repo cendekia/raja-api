d4aadf03970b0aa2acc8a0135f931e68f45a6cd4
Cendekia Pramana Putra <me@cendekiapp.com>
2017-10-01 17:08:36 +0700

    add village model and migration

---
+++ database/migrations/2017_10_01_100712_create_villages_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVillagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('villages', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('district_id')->unsigned();
+            $table->string('name');
+            $table->timestamps();
+
+            $table->foreign('district_id')->references('id')->on('districts')->onUpdate('cascade')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('villages');
+    }
+}
