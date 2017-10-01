95ceacf23a066466022c057a99851bb8af2781d1
Cendekia Pramana Putra <me@cendekiapp.com>
2017-10-01 17:53:04 +0700

    add relationship to district and village

--- app/Models/LocationRawData.php
+++ app/Models/LocationRawData.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class LocationRawData extends Model
 {
     protected $table = "location_raw_data";
+
+    public function district()
+    {
+    	return $this->hasOne(\App\Models\District::class, 'name', 'kecamatan');
+    }
+
+    public function village()
+    {
+    	return $this->hasOne(\App\Models\Village::class, 'name', 'kelurahan');
+    }
 }
