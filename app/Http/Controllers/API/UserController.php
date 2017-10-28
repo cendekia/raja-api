ec1c01e482937656bbee448656376f394fc6a69c
Cendekia Pramana Putra <me@cendekiapp.com>
2017-10-28 23:02:00 +0700

    remove dd

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -21,7 +21,6 @@ class UserController extends Controller
     public function login(){
         if(Auth::attempt(['email' => request('email'), 'password' => request('password')])){
             $user = Auth::user();
-            dd($user->createToken('MyApp'));
             $success['token'] =  $user->createToken('MyApp')->accessToken;
             return response()->json(['success' => $success], $this->successStatus);
         }
